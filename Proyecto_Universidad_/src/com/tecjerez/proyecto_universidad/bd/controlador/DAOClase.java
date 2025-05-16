2774528d33f2d80be0f32e9f2e59e9e21ac4ee01
Eduardo Alejo <eduardo.br@tecjerez.edu.mx>
2025-05-16 11:01:07 -0600

    DAOS de las tablas

---
+++ Proyecto_Universidad_/src/com/tecjerez/proyecto_universidad/bd/controlador/DAOClase.java
@@ -0,0 +1,5 @@
+package com.tecjerez.proyecto_universidad.bd.controlador;
+import com.tecjerez.proyecto_universidad.bd.modelo.Clase;
+
+public interface DAOClase extends DAO<Clase,Integer>{
+}
---
+++ Proyecto_Universidad_/src/com/tecjerez/proyecto_universidad/bd/controlador/DAOGarantia.java
@@ -0,0 +1,8 @@
+
+package com.tecjerez.proyecto_universidad.bd.controlador;
+
+import com.tecjerez.proyecto_universidad.bd.modelo.Garantia;
+
+public interface DAOGarantia extends DAO<Garantia, Integer>{
+    
+}
